cddf8939681a38e951d5c8ac5e6a479530f9896d
Srinithi-Rengasamy <srinithisamy346@gmail.com>
2025-07-15 11:21:10 +0530

    import route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import  Branches from './components/branches/branches';
 import  Gallery from './components/gallery/gallery';
 import  Contact from './components/contact/contact';
 import Videos from './components/videos/videos';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter , Route, Routes} from 'react-router-dom';
 function App() {
   return (
    <>
